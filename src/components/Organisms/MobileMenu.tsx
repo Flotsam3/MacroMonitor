be72957007fec07c7ab0316fb6f1901d16a315ed
ewg <ewg@dci.com>
2023-12-04 11:19:35 +0100

    feat:install styled-components to set overflow for body

--- src/components/Organisms/MobileMenu.tsx
+++ src/components/Organisms/MobileMenu.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import styles from "./MobileMenu.module.scss";
 import cog from "../../assets/images/cogwheel.png";
 import menu from "../../assets/images/burgerMenu.svg";
+import { createGlobalStyle } from "styled-components";
 
 export default function MobileMenu() {
     const [menuClass, setMenuClass] = useState(styles.mobileMenu);
@@ -13,8 +14,15 @@ export default function MobileMenu() {
         };
         setMenuClass(`${styles.mobileMenu} ${styles.slideMenu}`)
      };
+
+     const GlobalStyle = createGlobalStyle`
+      body {
+        overflow:hidden;
+      }
+     `
   return (
     <div className={styles.mobileWrapper}>
+        {menuClass.includes("slideMenu") && <GlobalStyle />}
         <div className={styles.burgerMenuWrapper}>
          <img onClick={handleMenu} src={menu} alt="Hamburger menu icon for mobile view" />
         </div>
